0d45f4ed7cbb3d517a356184fa76f11ff78734e0
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-22 14:59:35 +0530

    request.get

--- Demo/src/main/java/edu/SpringBoot/Demo/controller/Demo.java
+++ Demo/src/main/java/edu/SpringBoot/Demo/controller/Demo.java
@@ -1,6 +1,8 @@
 package edu.SpringBoot.Demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,16 @@ public class Demo {
 	public String m1() {
 		return "hi Sant";
 	}
-
+	
+	@RequestMapping(value="getSant",method = RequestMethod.GET)
+	public String m2() {
+		
+		return "Hi this is Sant";
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,value="getId")
+	
+	public String m3(@RequestParam int id) {
+		return "<h1> Id ="+id+"</h1>";
+	}
 }
