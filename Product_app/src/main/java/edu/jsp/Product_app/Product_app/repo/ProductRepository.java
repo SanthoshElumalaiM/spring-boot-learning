f1ba4763f551d56adb88f902fc4c69175c48d1c1
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-26 16:20:55 +0530

    productRepos

---
+++ Product_app/src/main/java/edu/jsp/Product_app/Product_app/repo/ProductRepository.java
@@ -0,0 +1,10 @@
+package edu.jsp.Product_app.Product_app.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.jsp.Product_app.Product_app.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+
+}
