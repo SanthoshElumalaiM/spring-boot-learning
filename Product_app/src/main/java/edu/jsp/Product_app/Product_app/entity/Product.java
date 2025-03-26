1d897b7ce86ce622c6e504de146d492c95141414
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-26 16:20:15 +0530

    ProductApp

---
+++ Product_app/src/main/java/edu/jsp/Product_app/Product_app/entity/Product.java
@@ -0,0 +1,51 @@
+package edu.jsp.Product_app.Product_app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long pid;
+	private  String name ;
+	private long quantity;
+	private double price;
+	private String color;
+	public long getPid() {
+		return pid;
+	}
+	public void setPid(long pid) {
+		this.pid = pid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(long quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+
+}
