8f7fb92dcfc0f769f47c7e2be0bdb2290a2afb83
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-26 15:27:22 +0530

    fetchById

--- Employee_app/src/main/java/edu/jsp/Employee_app/controller/EmployeeController.java
+++ Employee_app/src/main/java/edu/jsp/Employee_app/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package edu.jsp.Employee_app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.jsp.Employee_app.entity.Employee;
@@ -20,5 +22,11 @@ public class EmployeeController {
 	public Employee saveEmployee(@RequestBody Employee e)
 	{
 		return employeeService.saveEmployee(e); 
+	}
+	
+	@GetMapping("fetchById")
+	public Employee fetchById(@RequestParam long id)
+	{
+		return employeeService.fetchById(id);
 	}
 }
