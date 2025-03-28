76c7e0257b5f34921936a111186c8f8c98a5e64b
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-28 16:09:57 +0530

    JPAQuery

--- Employee_app/src/main/java/edu/jsp/Employee_app/repo/EmployeeRepository.java
+++ Employee_app/src/main/java/edu/jsp/Employee_app/repo/EmployeeRepository.java
@@ -1,6 +1,10 @@
 package edu.jsp.Employee_app.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import edu.jsp.Employee_app.entity.Employee;
 
@@ -12,4 +16,9 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	//findAll()
 	//deleteById(Long l)
 	
+	Employee findByName(String name);
+	
+	//name parameter
+	@Query(value="select e from Employee e where e.name like %:name%")
+	List<Employee> fecthByName(@Param("name") String name);
 }
