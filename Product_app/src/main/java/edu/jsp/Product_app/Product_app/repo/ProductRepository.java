b8dc85351edd400e3ef3b01c2fe236d373c8d0c7
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-28 16:33:05 +0530

    Query

--- Product_app/src/main/java/edu/jsp/Product_app/Product_app/repo/ProductRepository.java
+++ Product_app/src/main/java/edu/jsp/Product_app/Product_app/repo/ProductRepository.java
@@ -1,6 +1,10 @@
 package edu.jsp.Product_app.Product_app.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import edu.jsp.Product_app.Product_app.entity.Product;
 
@@ -8,4 +12,15 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 	
 
+	//save
+	//findById(id)
+	//findAll()
+	//deleteById(id)
+	//delete(Object o)
+	
+	Product findByName(String name);
+	
+	@Query(value="select p from Product p where p.price > :price")
+	List<Product> fecthByPrice(@Param("price") double price);
+	
 }
