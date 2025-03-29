c6fd62be2924ad92ff1edc004c4dd2ea0a166454
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-29 15:44:14 +0530

    Person

---
+++ Product_app/src/test/java/edu/jsp/Product_app/Product_app/ProductAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jsp.Product_app.Product_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
