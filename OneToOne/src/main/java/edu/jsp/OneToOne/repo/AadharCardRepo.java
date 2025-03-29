633ba94e2f9abd59598266fc756fe7a2d43dd978
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-03-29 15:04:05 +0530

    AadharCardRepo

---
+++ OneToOne/src/main/java/edu/jsp/OneToOne/repo/AadharCardRepo.java
@@ -0,0 +1,9 @@
+package edu.jsp.OneToOne.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.jsp.OneToOne.entity.AadharCard;
+
+public interface AadharCardRepo  extends JpaRepository<AadharCard, Integer>{
+
+}
