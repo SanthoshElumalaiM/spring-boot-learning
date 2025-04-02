529370aaac85305ab3049d9479097d6ddb873539
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-02 11:00:20 +0530

    UserController

--- Banking_App/src/main/java/edu/jsp/Banking_App/controller/UserController.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/controller/UserController.java
@@ -6,56 +6,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.jsp.Banking_App.entity.User;
-import edu.jsp.Banking_App.repo.UserRepo;
 import edu.jsp.Banking_App.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	
-	
+
 	@Autowired
 	private UserService userService;
 	
-	//based on request type it will be excuted
 	@GetMapping("/{id}")
-	public User getById(@PathVariable long id)
+	public User getByUserId(@PathVariable long id)
 	{
-		return userService.getById(id);
+		return userService.getByUserId(id);
 	}
 	
-	//http://localhost/users
-	@PutMapping()
-	public User createNewUser(User user)
+	@PostMapping()
+	public  User createNewUser(@RequestBody User user)
 	{
 		return userService.createNewUser(user);
 	}
 	
 	@PutMapping("/{id}")
-	public User updateUserById(@PathVariable long id,User user)
+	public User updateUserById(@PathVariable long id, @RequestBody  User newUser)
 	{
-		return userService.updateUserById(id, user);
+		return userService.UpdateByUserId(id, newUser);
 	}
 	
 	@DeleteMapping("/{id}")
-	public String deleteById(@PathVariable long id)
+	public String deleteByUserId(@PathVariable long id)
 	{
-		return userService.deleteById(id);
+		return userService.deleteByUserId(id);
 	}
 	
-	@GetMapping("/search")
-	public List<User> getByUserName(@RequestParam String name)
-	{
-		return userService.getUserByName(name);
+	@GetMapping("/search/{name}")
+	public List<User> getByUserName(@PathVariable String name){
+		return userService.getByUserName(name);
 	}
-	
-	
-	
-
 }
+
