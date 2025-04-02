edae6f0096be6834eae2c244df4fdc6c78e70a3f
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-02 15:17:07 +0530

    loan

---
+++ Banking_App/src/main/java/edu/jsp/Banking_App/entity/Loan.java
@@ -0,0 +1,86 @@
+package edu.jsp.Banking_App.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class Loan {
+
+	private  long id;
+	private double amount;
+	private LocalDate applyDate;
+	private LocalDate approvalDate;
+	private LocalDate disbursementDate;
+	private double balance;
+	private String status;//applied,approved,disburse,repaid
+	
+	@ManyToOne
+	@JoinColumn
+	private User user;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getApplyDate() {
+		return applyDate;
+	}
+
+	public void setApplyDate(LocalDate applyDate) {
+		this.applyDate = applyDate;
+	}
+
+	public LocalDate getApprovalDate() {
+		return approvalDate;
+	}
+
+	public void setApprovalDate(LocalDate approvalDate) {
+		this.approvalDate = approvalDate;
+	}
+
+	public LocalDate getDisbursementDate() {
+		return disbursementDate;
+	}
+
+	public void setDisbursementDate(LocalDate disbursementDate) {
+		this.disbursementDate = disbursementDate;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}
