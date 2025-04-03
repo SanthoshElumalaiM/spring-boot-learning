98dd0b1cadaf39ea3ccb40d2715d8c40f12895da
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-03 15:23:03 +0530

    getStatus

--- Banking_App/src/main/java/edu/jsp/Banking_App/controller/LoanController.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/controller/LoanController.java
@@ -1,5 +1,7 @@
 package edu.jsp.Banking_App.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,36 @@ public class LoanController {
 			return  loanService.applyLoan(userId, loan);
 	}
 	
+	public Loan getLoan(long loanId)
+	{
+		return loanService.getLoanById(loanId);
+	}
+	
+	public Loan updateLoan(long loanId,Loan loan)
+	{
+		return loanService.updateLoan(loanId, loan);
+	}
+	
+	public String deleteById(long userId,long loanId)
+	{
+		return loanService.deleteLoanById(userId, loanId);
+	}
+	
+	public Set<Loan> fetchAllLoan(long userId)
+	{
+		return loanService.fetchAllLoan(userId);
+	}
+	
+	public Loan repayLoan(long loanId,double amount) {
+		
+		return loanService.repayLoan(loanId, amount);
+		
+	}
+	
+	public String loanStaus(long loanId)
+	{
+		return loanService.loanStatus(loanId);
+	}
 	
 	
 	
