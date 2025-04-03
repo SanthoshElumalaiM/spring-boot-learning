b548748a603ee0fa7f2b10c68b5db3810d58ee4f
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-03 16:34:10 +0530

    @Entity

--- Banking_App/src/main/java/edu/jsp/Banking_App/entity/Loan.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/entity/Loan.java
@@ -2,12 +2,20 @@ package edu.jsp.Banking_App.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Loan {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private  long id;
+	
 	private double amount;
 	private LocalDate applyDate;
 	private LocalDate approvalDate;
