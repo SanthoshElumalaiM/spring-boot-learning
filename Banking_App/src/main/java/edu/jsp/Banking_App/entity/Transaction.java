0a683818b2dbaa2b72b610aa53c2cb8477544bb6
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-04 18:12:03 +0530

    Transaction

---
+++ Banking_App/src/main/java/edu/jsp/Banking_App/entity/Transaction.java
@@ -0,0 +1,43 @@
+package edu.jsp.Banking_App.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Transaction {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long transactionId;
+	private double amount;
+	private String date;
+	private String status;
+	public long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
