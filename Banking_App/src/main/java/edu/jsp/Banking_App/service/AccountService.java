87af3d82b6e5d82dfe569885b5afddadabc098b4
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-05 15:23:45 +0530

    AccountService

---
+++ Banking_App/src/main/java/edu/jsp/Banking_App/service/AccountService.java
@@ -0,0 +1,14 @@
+package edu.jsp.Banking_App.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.jsp.Banking_App.repo.AccountRepo;
+
+@Service
+public class AccountService {
+
+	
+	@Autowired
+	private AccountRepo accountRepo;
+}
