7a350d84c048730900ffc5d8b947014918bca930
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-05 15:57:59 +0530

    findByAccountUserUidAndDateBetween

--- Banking_App/src/main/java/edu/jsp/Banking_App/repo/TransactionRepo.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/repo/TransactionRepo.java
@@ -1,9 +1,14 @@
 package edu.jsp.Banking_App.repo;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.jsp.Banking_App.entity.Transaction;
+import edu.jsp.Banking_App.entity.User;
 
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
 
+	List<Transaction> findByAccountUserUidAndDateBetween(User userId,LocalDateTime start,LocalDateTime end);
 }
