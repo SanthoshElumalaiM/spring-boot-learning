10322b4b5323f9cb40546a3e308da863a43933bb
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-07 15:59:12 +0530

    findByAccountUserUid

--- Banking_App/src/main/java/edu/jsp/Banking_App/repo/TransactionRepo.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/repo/TransactionRepo.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import edu.jsp.Banking_App.entity.Transaction;
 import edu.jsp.Banking_App.entity.User;
@@ -11,4 +12,7 @@ import edu.jsp.Banking_App.entity.User;
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
 
 	List<Transaction> findByAccountUserUidAndDateBetween(User userId,LocalDateTime start,LocalDateTime end);
+	
+	@Query("select t from  transaction  t where t.account.user.uid =?1 order by date")
+	List<Transaction> findByAccountUserUid(long userId);
 }
