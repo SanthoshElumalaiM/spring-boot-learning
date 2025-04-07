0103144227dc910993ccfce56abdda5b3dfbeda7
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-07 15:29:52 +0530

    createAccount

--- Banking_App/src/main/java/edu/jsp/Banking_App/service/AccountService.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/service/AccountService.java
@@ -3,7 +3,10 @@ package edu.jsp.Banking_App.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.jsp.Banking_App.entity.Account;
+import edu.jsp.Banking_App.entity.User;
 import edu.jsp.Banking_App.repo.AccountRepo;
+import edu.jsp.Banking_App.repo.UserRepo;
 
 @Service
 public class AccountService {
@@ -12,7 +15,20 @@ public class AccountService {
 	@Autowired
 	private AccountRepo accountRepo;
 	
+	@Autowired
+	private UserRepo userRepo;
 	
 	
+	public Account createAccount(long userId,Account account)
+	{
+		
+		User user= userRepo.findById(userId).orElse(null);
+		
+		if(user!=null)
+		{
+		
+		}
+		
+	}
 	
 }
