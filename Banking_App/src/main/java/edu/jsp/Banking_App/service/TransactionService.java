9031bf616c7f9ccecaba554817f25500ed291196
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-07 15:14:26 +0530

    AddTransaction

--- Banking_App/src/main/java/edu/jsp/Banking_App/service/TransactionService.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/service/TransactionService.java
@@ -3,6 +3,9 @@ package edu.jsp.Banking_App.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.jsp.Banking_App.entity.Account;
+import edu.jsp.Banking_App.entity.Transaction;
+import edu.jsp.Banking_App.repo.AccountRepo;
 import edu.jsp.Banking_App.repo.TransactionRepo;
 
 @Service
@@ -11,5 +14,21 @@ public class TransactionService {
 	@Autowired
 	private TransactionRepo transactionRepo;
 	
+	@Autowired
+	private AccountRepo accountRepo;
+	
+	
+	public Transaction addTransaction(Long accountId,Transaction transaction)
+	{
+		  
+		Account account= accountRepo.findById(accountId).orElse(null);
+		
+		if(account!=null) {
+			account.addTransaction(transaction);
+			accountRepo.save(account);
+			return transaction;
+		}
+		return null;
+	}
 	
 }
