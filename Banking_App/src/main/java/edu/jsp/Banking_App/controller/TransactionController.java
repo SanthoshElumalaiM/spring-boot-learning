c1a7c5996bfd6af567d69bad92e19e5202ce3717
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-07 15:35:54 +0530

    addTransaction

--- Banking_App/src/main/java/edu/jsp/Banking_App/controller/TransactionController.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/controller/TransactionController.java
@@ -1,8 +1,12 @@
 package edu.jsp.Banking_App.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.jsp.Banking_App.entity.Transaction;
 import edu.jsp.Banking_App.service.TransactionService;
 
 @RestController
@@ -10,5 +14,11 @@ public class TransactionController {
 	
 	@Autowired
 	private TransactionService transactionService;
+	
+	@PostMapping("transaction/account/{accountId}")
+	public Transaction addTransaction(@PathVariable long accountId,@RequestBody Transaction transaction)
+	{
+		return transactionService.addTransaction(accountId, transaction);
+	}
 
 }
