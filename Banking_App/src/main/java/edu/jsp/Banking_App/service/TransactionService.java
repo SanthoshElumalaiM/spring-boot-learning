a9e0a8fd353ac0ad0a9cfcfc0aeb7416a3854c5c
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-08 14:57:01 +0530

    createTransaction

--- Banking_App/src/main/java/edu/jsp/Banking_App/service/TransactionService.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/service/TransactionService.java
@@ -1,5 +1,7 @@
 package edu.jsp.Banking_App.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +31,11 @@ public class TransactionService {
 			return transaction;
 		}
 		return null;
+	}
+	
+	public List<Transaction> getTransactionAsc(long userId)
+	{
+		return transactionRepo.findByAccountUserUid(userId);
 	}
 	
 }
