6a3102c68760aa8b83a0a4fa303cb3ec7d369b6f
SanthoshElumalaiM <santhoshelumalaim@gmail.com>
2025-04-08 16:58:24 +0530

    getBalance()

--- Banking_App/src/main/java/edu/jsp/Banking_App/service/AccountService.java
+++ Banking_App/src/main/java/edu/jsp/Banking_App/service/AccountService.java
@@ -1,5 +1,7 @@
 package edu.jsp.Banking_App.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,9 +28,48 @@ public class AccountService {
 		
 		if(user!=null)
 		{
-		 return account;
+		  user.addAccount(account);
+		  userRepo.save(user);
+		  return account;
 		}
 		return null;
 	}
 	
+	public Account updateAccount(long accountId,Account newAccount)
+	{
+		Account exAccount= accountRepo.findById(accountId).orElse(null);
+		
+		if(exAccount!=null)
+		{
+			newAccount.setId(accountId);
+			return accountRepo.save(newAccount);
+		}
+		return null;
+			
+	}
+	
+	public String deleteAccountById(long userId,long accountId)
+	{
+		User user=  userRepo.findById(userId).orElse(null);
+		Account account= accountRepo.findById(accountId).orElse(null);
+		
+		if(user!=null)
+		{
+			user.removeAccont(account);
+			accountRepo.delete(account);
+			return "Account deleted ";
+		}
+		return "Id Not found";
+	}
+	
+	public List<Account> getAccountById(long userId)
+	{
+		return accountRepo.getAccountByUserId(userId);
+	}
+	
+	public double accountBalance(long accountId)
+	{
+		Account account= accountRepo.findById(accountId).orElse(null);
+		return account.getBalance();
+	}
 }
